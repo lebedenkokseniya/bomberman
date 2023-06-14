ca5d0bd6142fdcb03336d05bb99a7c20d16be099
lebedenkokseniya <128731966+lebedenkokseniya@users.noreply.github.com>
2023-06-14 22:55:04 +0200

    Main.cs

--- bomberman/Main.cs
+++ bomberman/Main.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
 using System.Diagnostics;
-using System.Threading;
-using System.Numerics;
 
 class Global
 {
@@ -27,23 +23,19 @@ public class Game
         GameProcess();
     }
 
-    int totalSeconds = 120;
+    int totalSeconds = 180;
     Stopwatch stopwatch = new Stopwatch();
     public void GameProcess()
     {
+        stopwatch.Start();
         do
         {
-            stopwatch.Start();
-            do
-            {
                 field.DrawField();
                 TimeSpan remainingTime = TimeSpan.FromSeconds(totalSeconds - stopwatch.Elapsed.TotalSeconds);
                 Console.SetCursorPosition(20, 15);
                 Console.WriteLine(remainingTime.ToString("mm\\:ss"));
-                state.GameFinished = Input(GetField());
-            } while (stopwatch.Elapsed.TotalSeconds <= 120);
-            state.GameFinished = true;
-        } while (!state.GameFinished);
+                Input(GetField(), state);
+        } while (!state.GameFinished && stopwatch.Elapsed.TotalSeconds <= totalSeconds);
         Console.Clear();
         if (state.Win)
         {
@@ -68,11 +60,13 @@ public class Game
             }
         }
     }
+
     private Field GetField()
     {
         return field;
     }
-    private bool Input(Field field)
+
+    private void Input(Field field, State state)
     {
         ConsoleKeyInfo inputedKey = Console.ReadKey();
         player.Action(field, player, inputedKey);
@@ -83,6 +77,5 @@ public class Game
         }
         Bomb bomb = new Bomb();
         bomb.Action(field, player.y, player.x, inputedKey, state);
-        return state.GameFinished;
     }
 }
